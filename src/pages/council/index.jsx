b866766085173c2c0e599dd33c9ed3f018339629
ishantSikdar <ishant9715@gmail.com>
2024-04-28 00:30:47 +0530

    Clean up

--- src/pages/council/index.jsx
+++ src/pages/council/index.jsx
@@ -1,5 +1,3 @@
 export default function CouncilPage() {
-    return <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum nihil qui, quibusdam quam culpa inventore cum officiis atque tempore dolore. Aperiam, iure? Mollitia ipsam quam eaque quod. Optio, dolor tenetur?
-        Council Page
-    </div>
+    return <div></div>
 }
