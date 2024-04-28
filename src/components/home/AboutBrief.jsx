15d2e9b5a8a7e39bcfa0ac32539dce146d7c71df
ishantSikdar <ishant9715@gmail.com>
2024-04-28 13:35:17 +0530

    Fix About Brief Page

--- src/components/home/AboutBrief.jsx
+++ src/components/home/AboutBrief.jsx
@@ -1,11 +1,15 @@
 export default function AboutBrief() {
   return (
-    <div className="flex justify-center items-center">
-      <div className="my-auto absolute bg-white p-10 flex flex-col gap-3">
+    <div className="flex md:h-screen justify-center items-center">
+      {/* Render this image only on tablet and desktop screens */}
+      <div className="hidden md:block w-[50%] h-full">
+        <img src="/about_brief.png" alt="About" className="w-full h-full" />
+      </div>
+      <div className="my-auto md:w-[50%] bg-white p-16 md:px-20 lg:px-36 flex flex-col gap-3">
         <h2 className="uppercase font-bold text-3xl">About the Alumni Association</h2>
         <p className="font-light text-lg">Established in 1920, University of El Dorado has long been a home to critical thinking, progressive education, and a conscientious community. This private non-profit university is committed to pushing positive change and creating a caring world for everyone.</p>
         <a href="#" className="uppercase underline text-[#4b39b5] font-bold">Read More</a>
       </div>
     </div>
-  )
+  );
 }
--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -16,8 +16,8 @@ export default function HeroSection() {
 
 			<div className="z-10">
 				<div className="uppercase font-medium text-lg md:text-2xl">Welcome to the</div>
-				<h1 className="uppercase font-bold text-3xl md:text-5xl lg:text-7xl my-2 md:my-3">Alumni Association</h1>
-				<p className="text-sm md:text-xl leading-5 mx-5 lg:mx-40">
+				<h1 className="uppercase font-bold text-3xl md:text-5xl lg:text-6xl my-2 md:my-3">Alumni Association</h1>
+				<p className="text-sm md:text-lg leading-5 mx-5 lg:mx-40">
 					Established in 1920, University of El Dorado has long been a home to critical thinking, progressive education, and a conscientious community. This private non-profit university is committed to pushing positive change and creating a caring world for everyone.
 				</p>
 				<button className="w-max mx-auto my-3 md:my-5 uppercase px-12 py-4 bg-[#4b39b5] md:text-xl">
