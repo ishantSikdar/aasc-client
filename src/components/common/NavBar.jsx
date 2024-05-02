30334c5e4b2fe9ef08ddb64b16c07e7bc50e46f9
ishantSikdar <ishant9715@gmail.com>
2024-05-02 22:18:09 +0530

    Fonts Added, NavBar Council Button Updated

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
-import { ROUTE_ABOUT, ROUTE_COUNCIL, ROUTE_EVENTS, ROUTE_GALLERY, ROUTE_HOME } from "../../constants/routes";
+import { ROUTE_ABOUT, ROUTE_EXE_COUNCIL, ROUTE_EVENTS, ROUTE_GALLERY, ROUTE_HOME, ROUTE_AASC_COUNCIL } from "../../constants/routes";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { useState } from "react";
 
 export default function NavBar({ isMobile }) {
   const [showMenu, setShowMenu] = useState(false);
+  const [showCouncil, setShowCouncil] = useState(false);
 
   const toggleMobileMenu = () => {
     setShowMenu(prevState => !prevState);
@@ -15,6 +16,10 @@ export default function NavBar({ isMobile }) {
     setShowMenu(false);
   }
 
+  const toggleCouncil = () => {
+    setShowCouncil(prev => !prev)
+  }
+
   return (
     <div className="z-50 fixed top-0 left-0 right-0 flex justify-between bg-white text-black h-16 md:h-20 opacity-90 p-4">
       <Link to={ROUTE_HOME} className="flex items-center" onClick={closeMobileMenu}>
@@ -22,12 +27,20 @@ export default function NavBar({ isMobile }) {
       </Link>
 
       {/* For Tab/Desktop screen */}
-      {!isMobile && <div className="flex justify-between items-center px-7 lg:px-10 gap-14 text-xl font-bold">
+      {!isMobile && <div className="relative flex justify-between items-center px-7 lg:px-10 gap-14 text-xl font-medium uppercase">
         <Link to={ROUTE_ABOUT}>About Us</Link>
-        <Link to={ROUTE_COUNCIL}>Council</Link>
+        <button onClick={toggleCouncil} className="relative uppercase">
+          Council
+          {!isMobile && showCouncil && <div className="absolute p-3 w-52 flex flex-col gap-2 bg-gray-100 top-10 right-[-75%] text-sm">
+            <Link to={ROUTE_EXE_COUNCIL}>Executive Council</Link>
+            <div className="w-full h-[1pt] bg-[#252525]"></div>
+            <Link to={ROUTE_AASC_COUNCIL}>AASC Council</Link>
+          </div>}
+        </button>
         <Link to={ROUTE_EVENTS}>Events</Link>
         <Link to={ROUTE_GALLERY}>Gallery</Link>
       </div>}
+
 
       {/* Hamburger button to enable Mobile menu, only displays on mobile size res */}
       {isMobile && <button onClick={toggleMobileMenu} className={`z-10 text-black`}>
@@ -39,7 +52,8 @@ export default function NavBar({ isMobile }) {
         <div className="mt-20 flex flex-col gap-1 text-right p-4 font-light uppercase text-lg">
           <Link to={ROUTE_HOME} onClick={closeMobileMenu} className="border-b-[1pt] border-[#252525] p-2">Home</Link>
           <Link to={ROUTE_ABOUT} onClick={closeMobileMenu} className="border-b-[1pt] border-[#252525] p-2">About Us</Link>
-          <Link to={ROUTE_COUNCIL} onClick={closeMobileMenu} className="border-b-[1pt] border-[#252525] p-2">Council</Link>
+          <Link to={ROUTE_EXE_COUNCIL} onClick={closeMobileMenu} className="border-b-[1pt] border-[#252525] p-2">Executive Council</Link>
+          <Link to={ROUTE_AASC_COUNCIL} onClick={closeMobileMenu} className="border-b-[1pt] border-[#252525] p-2">AASC Council</Link>
           <Link to={ROUTE_EVENTS} onClick={closeMobileMenu} className="border-b-[1pt] border-[#252525] p-2">Events</Link>
           <Link to={ROUTE_GALLERY} onClick={closeMobileMenu} className="border-b-[1pt] border-[#252525] p-2">Gallery</Link>
         </div>
--- src/constants/routes/index.js
+++ src/constants/routes/index.js
@@ -3,7 +3,8 @@ import HomePage from "../../pages/home";
 
 export const ROUTE_HOME = "/";
 export const ROUTE_ABOUT = "/about";
-export const ROUTE_COUNCIL = "/council";
+export const ROUTE_EXE_COUNCIL = "/exe-council";
+export const ROUTE_AASC_COUNCIL = "/aasc-council";
 export const ROUTE_EVENTS = "/events";
 export const ROUTE_GALLERY = "/gallery";
 export const ROUTE_ANY = "*";
@@ -17,7 +18,7 @@ export const APP_ROUTES = [
         element: HomePage
     },{
         id: 2,
-        path: ROUTE_COUNCIL,
+        path: ROUTE_EXE_COUNCIL,
         element: CouncilPage
     }
 
