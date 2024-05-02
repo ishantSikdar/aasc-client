d6327b0562d037c8c9a6cda56af8c9b9c15297d2
Shushant-Priyadarshi <profshushant@gmail.com>
2024-05-02 22:50:01 +0530

    about page done

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,15 @@
+
+
 import AboutBrief from "../../components/home/AboutBrief";
 import HeroSection from "../../components/home/HeroSection";
+
 
 export default function HomePage({ isMobile }) {
   return (
     <div>
       <HeroSection  />
       <AboutBrief isMobile={isMobile}/>
+      
     </div>
   )
 }
