88f805d6e7304430c7357960c0b3ab7389068e76
ishantSikdar <ishant9715@gmail.com>
2024-05-03 18:37:53 +0530

    Resize desktop size AASC Council

--- src/components/common/StudentCouncilCard.jsx
+++ src/components/common/StudentCouncilCard.jsx
@@ -30,15 +30,15 @@ export default function StudentCouncilCard({ photo, isMobile, name, position, li
     }, [isMobile]);
 
     return (
-        <div ref={elementRef} className="relative w-[300px] h-[410px] bg-[#EEEBE8] flex flex-col justify-between items-center overflow-hidden group">
+        <div ref={elementRef} className="relative w-[300px] h-[410px] xl:w-[200px] xl:h-[310px] bg-[#EEEBE8] flex flex-col justify-between items-center overflow-hidden group">
             <img src={photo} alt={name} className="absolute w-full h-full object-cover" />
-            <div className={`absolute top-0 flex flex-col gap-2 text-white text-4xl p-2 transition-all duration-300 ${isInView ? 'right-0' : '-right-14'} ${!isMobile ? 'group-hover:right-0' : '-right-14'}`}>
-                <a href={instagram}><img src={`/instagram.png`} alt={instagram} className="w-10" /></a>
-                <a href={linkedin}><img src={`/linkedin.png`} alt={linkedin} className="w-10" /></a>
+            <div className={`absolute top-0 flex flex-col gap-2 text-white p-2 transition-all duration-300 ${isInView ? 'right-0' : '-right-14'} ${!isMobile ? 'group-hover:right-0' : '-right-14'}`}>
+                <a href={instagram}><img src={`/instagram.png`} alt={instagram} className="w-10 xl:w-7" /></a>
+                <a href={linkedin}><img src={`/linkedin.png`} alt={linkedin} className="w-10 xl:w-7" /></a>
             </div>
-            <div className="absolute bottom-2 right-2 flex flex-col items-end bg-[#f5f5f5]/[0.6] p-2">
-                <h3 className="text-[#853333] font-bold text-3xl">{name}</h3>
-                <div className="uppercase text-xl font-extrabold">{position}</div>
+            <div className="absolute bottom-1 right-1 xl:bottom-0 xl:right-0 flex flex-col items-end bg-[#f5f5f5]/[0.6] p-2 text-center xl:w-full">
+                <h3 className="text-[#853333] font-bold text-3xl xl:text-xl">{name}</h3>
+                <div className="uppercase text-xl font-extrabold xl:text-sm">{position}</div>
             </div>
         </div>
     )
--- src/pages/aascCouncil/index.jsx
+++ src/pages/aascCouncil/index.jsx
@@ -2,13 +2,14 @@ import StudentCouncilCard from "../../components/common/StudentCouncilCard";
 import STUDENT_COUNCIL from "../../constants/aascCouncil/studentCouncilDetails.js";
 
 export default function AascCouncilPage({ isMobile }) {
-    return <div className="my-20 flex flex-col gap-10 md:px-10 xl:px-52">
-        <div className="text-[#853333] text-center  flex flex-col items-center gap-2">
+    // const shuffledStudentCouncil = STUDENT_COUNCIL.sort(() => Math.random() - 0.5);
+    return <div className="my-20 flex flex-col gap-10 md:px-10">
+        <div className="text-[#853333] text-center  flex flex-col items-center gap-2 md:mt-10">
             <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold uppercase">Alumni Association Student Council</h2>
             <div className="h-1 w-[70%] lg:w-[58%]  bg-[#853333]"></div>
         </div>
 
-        <div className="flex flex-col md:flex-row flex-wrap gap-10 md:gap-16 justify-center items-center  xl:items-end">
+        <div className="flex flex-col md:flex-row flex-wrap gap-10 md:gap-16 xl:gap-5 justify-center items-center">
             {STUDENT_COUNCIL.map((student) => (
                 <StudentCouncilCard
                     key={student.id}
