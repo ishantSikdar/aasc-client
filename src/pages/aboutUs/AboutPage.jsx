74f286b3a908a27a18ab572527d46aa8023d31eb
ishantSikdar <ishant9715@gmail.com>
2024-05-05 14:15:57 +0530

    WIP

--- src/pages/aboutUs/AboutPage.jsx
+++ src/pages/aboutUs/AboutPage.jsx
@@ -1,92 +1,90 @@
 function AboutPage() {
   return (
-    <>
-      <div className="p-4 md:p-16 mt-16">
-        <div className=" w-full md:max-w-screen-2xl  mx-auto md:px-20 px-4  bg-[#eae9e9]">
-          <p className="md:text-lg md:p-8 text-center">
-            The Alumni Association has been created with the vision of fostering
-            lifelong connections and celebrating the achievements of our
-            esteemed alumni. We serve as a bridge between past, present, and
-            future, providing a platform for networking, mentorship, and
-            professional growth. We endeavour to foster the legacy of our alma
-            mater while also championing the diverse
-            accomplishments of our alumni.
-          </p>
-        </div>
-        <div className="mt-8 md:mt-16  md:grid grid-cols-2 gap-20">
-          <div>
-            <h1 className="text-2xl font-bold text-[#8c2727] col-span-1">
-              Vision
-            </h1>
-            <hr className=" border-[#8c2727] border-solid border-2"></hr>
-            <p className="md:text-lg mt-1">
-              The vision of an Alumni Association Student Council is to forge a lasting bond between current students and alumni, offering mentorship, networking, and support. It strives to empower students through career guidance, events, and a culture of giving back, fostering a dynamic connection that strengthens both the present and future of the institution.
-            </p>
-          </div>
-          <div className="mt-4 md:mt-0">
-            <h1 className="text-2xl font-bold text-[#8c2727] col-span-1 ">
-              Mission
-            </h1>
-            <hr className=" border-[#8c2727] border-solid border-2"></hr>
-            <p className="md:text-lg mt-1">
-              This private non-profit university is committed to pushing
-              positive change and creating a caring world for
-              everyone.Established in 1920, University of El Dorado has long
-              been a home to critical thinking, progressive education, and a
-              conscientious community. This private non-profit university is
-              committed to pushing positive change and creating a caring world
-              for everyone.
-            </p>
-          </div>
-        </div>
+    <div className="p-4 md:p-16 mt-16">
+      <div className=" w-full md:max-w-screen-2xl  mx-auto md:px-20 px-4  bg-[#eae9e9]">
+        <p className="md:text-lg md:p-8 text-center">
+          The Alumni Association has been created with the vision of fostering
+          lifelong connections and celebrating the achievements of our
+          esteemed alumni. We serve as a bridge between past, present, and
+          future, providing a platform for networking, mentorship, and
+          professional growth. We endeavour to foster the legacy of our alma
+          mater while also championing the diverse
+          accomplishments of our alumni.
+        </p>
+      </div>
+      <div className="mt-8 md:mt-16  md:grid grid-cols-2 gap-20">
         <div>
-          <h1 className="text-2xl font-bold text-[#8c2727] mt-8 md:mt-16">
-            Objectives
+          <h1 className="text-2xl font-bold text-[#8c2727] col-span-1">
+            Vision
           </h1>
-          <hr className=" border-[#8c2727] border-solid border-2 w-1/2"></hr>
+          <hr className=" border-[#8c2727] border-solid border-2"></hr>
           <p className="md:text-lg mt-1">
-            This private non-profit university is committed to pushing positive
-            change and creating a caring world for everyone.Established in 1920,
-            University of El Dorado has long been a home to critical thinking,
-            progressive education, and a conscientious community. This private
-            non-profit university is committed to pushing positive change and
-            creating a caring world for everyone.
+            The vision of an Alumni Association Student Council is to forge a lasting bond between current students and alumni, offering mentorship, networking, and support. It strives to empower students through career guidance, events, and a culture of giving back, fostering a dynamic connection that strengthens both the present and future of the institution.
           </p>
-          <div className="m-6 ">
-            <ul className="list-disc md:text-lg">
-              <li>
-                This private non-profit university is committed to pushing
-                positive change and creating a caring world for everyone
-              </li>
-              <li>
-                This private non-profit university is committed to pushing
-                positive change and creating a caring world for everyone
-              </li>
-              <li>
-                This private non-profit university is committed to pushing
-                positive change and creating a caring world for everyone
-              </li>
-              <li>
-                This private non-profit university is committed to pushing
-                positive change and creating a caring world for everyone
-              </li>
-              <li>
-                This private non-profit university is committed to pushing
-                positive change and creating a caring world for everyone
-              </li>
-              <li>
-                This private non-profit university is committed to pushing
-                positive change and creating a caring world for everyone
-              </li>
-              <li>
-                This private non-profit university is committed to pushing
-                positive change and creating a caring world for everyone
-              </li>
-            </ul>
-          </div>
+        </div>
+        <div className="mt-4 md:mt-0">
+          <h1 className="text-2xl font-bold text-[#8c2727] col-span-1 ">
+            Mission
+          </h1>
+          <hr className=" border-[#8c2727] border-solid border-2"></hr>
+          <p className="md:text-lg mt-1">
+            This private non-profit university is committed to pushing
+            positive change and creating a caring world for
+            everyone.Established in 1920, University of El Dorado has long
+            been a home to critical thinking, progressive education, and a
+            conscientious community. This private non-profit university is
+            committed to pushing positive change and creating a caring world
+            for everyone.
+          </p>
         </div>
       </div>
-    </>
+      <div>
+        <h1 className="text-2xl font-bold text-[#8c2727] mt-8 md:mt-16">
+          Objectives
+        </h1>
+        <hr className=" border-[#8c2727] border-solid border-2 w-1/2"></hr>
+        <p className="md:text-lg mt-1">
+          This private non-profit university is committed to pushing positive
+          change and creating a caring world for everyone.Established in 1920,
+          University of El Dorado has long been a home to critical thinking,
+          progressive education, and a conscientious community. This private
+          non-profit university is committed to pushing positive change and
+          creating a caring world for everyone.
+        </p>
+        <div className="m-6 ">
+          <ul className="list-disc md:text-lg">
+            <li>
+              This private non-profit university is committed to pushing
+              positive change and creating a caring world for everyone
+            </li>
+            <li>
+              This private non-profit university is committed to pushing
+              positive change and creating a caring world for everyone
+            </li>
+            <li>
+              This private non-profit university is committed to pushing
+              positive change and creating a caring world for everyone
+            </li>
+            <li>
+              This private non-profit university is committed to pushing
+              positive change and creating a caring world for everyone
+            </li>
+            <li>
+              This private non-profit university is committed to pushing
+              positive change and creating a caring world for everyone
+            </li>
+            <li>
+              This private non-profit university is committed to pushing
+              positive change and creating a caring world for everyone
+            </li>
+            <li>
+              This private non-profit university is committed to pushing
+              positive change and creating a caring world for everyone
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
 
