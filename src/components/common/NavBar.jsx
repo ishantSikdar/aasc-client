76e70d0e89b8728323a6dfeb0b68ff73c73409cb
msr8 <live.shashwat@gmail.com>
2025-02-19 01:18:41 +0530

    Removed the "executive community" section from navbar

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -64,8 +64,8 @@ export default function NavBar({ isMobile }) {
             Council
             {!isMobile && showCouncil && (
               <div className="absolute p-3 w-52 flex flex-col gap-2 shadow-md bg-white top-10 right-[-75%] text-sm">
-                <Link to={ROUTE_EXE_COUNCIL}>Executive Council</Link>
-                <div className="w-full h-[0.5pt] bg-[#252525]"></div>
+                {/* <Link to={ROUTE_EXE_COUNCIL}>Executive Council</Link> */}
+                {/* <div className="w-full h-[0.5pt] bg-[#252525]"></div> */}
                 <Link to={ROUTE_AASC_COUNCIL}>AASC Council</Link>
                 <div className="w-full h-[0.5pt] bg-[#252525]"></div>
                 <Link to={ROUTE_AASC_COMMITTEE}>AASC Committee</Link>
